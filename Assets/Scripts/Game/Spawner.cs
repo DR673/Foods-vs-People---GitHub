d1e44d391481bea3548bc76e05c581f63e03a379
DR673 <DR673@live.mdx.ac.uk>
2022-11-22 19:00:19 +0000

    First Commit

---
+++ Assets/Scripts/Game/Spawner.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    // List of foods (prefabs) that will be instantiated
+    int spawnID = -1;
+
+    // ID of tower that is going to spawn
+    public List<GameObject> towers;
+
+
+}
